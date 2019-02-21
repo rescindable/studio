964e4ee4416ec5eeac92b4184f9979c672bb6006
Mweya Ruider <mweyaruider@gmail.com>
2019-02-21 22:31:20 +0200

    Automatic push cuz ya boi's of the lazy type

--- Push.sh
+++ Push.sh
@@ -2,6 +2,6 @@ git fetch
 git checkout master
 git add .
 git commit -m "Automatic push cuz ya boi's of the lazy type"
-git push
-bash Frontend/Push.sh
+git push master
 bash Backend/Push.sh
+bash Frontend/Push.sh
