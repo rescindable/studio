9cfcb19c632dab7f87712657a909461271441075
Mweya Ruider <mweyaruider@gmail.com>
2019-02-22 16:45:16 +0200

    Automatic push cuz ya bois a lazy as heck

--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -1,28 +1,16 @@
-// Main JS
-function prevWeek() {
-	console.log('prevWeek');
-}
-
-// Define components
-Vue.component('btn-scroll-left', {
-	data: function() {},
-	template: '<button v-on:click="prevWeek();">Previous Week</button>'
-})
-
-// Define instance
 var app = new Vue({
-	el: '#body',
-	mouned: function() {
-		this.init();
+	el: '#pageWrapper',
+	data: {
+		message: "This is Vue"
 	},
 	methods: {
-		init:function () {
-			fetch('/skeleton').then(
-			function(response) {
-				return response;
-			}).then(function(pageskeleton){
-				document.getElementById('body').innerHTML = pageSkeleton;
-			})
+		reverseMessage: function() {
+			this.message = this.message.split('').reverse().join('')
+		}
+		bookSlot: function() {
+			// Get info from
+			// Send POST here
+			
 		}
 	}
-})
+});
